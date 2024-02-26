c294391e01a2a3fd95a3dd23b1f6c87c93372b8b
priyanshur66 <priyanshuranjan0001@gmail.com>
2024-02-26 15:30:47 +0530

    contract str change

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./GreenWim.sol";
-import "./APIVerify.sol";
-import "./APICreditRefresh.sol";
-import "./GWToken.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // interface IAPIVerify {
@@ -13,7 +9,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Registry {
     //APIVerify public apiVerify;
-   // GWToken public gwToken;
+    // GWToken public gwToken;
     IERC20 public usdtToken;
 
     struct Order {
@@ -31,40 +27,41 @@ contract Registry {
         uint noOfGWTokens;
     }
 
-
-
-
-
     Order[] public orderArray;
-   // Lease[] public leaseArray;
-   // Bid[] public bidsArray;
-   // GreenWim[] public contracts;
+    // Lease[] public leaseArray;
+    // Bid[] public bidsArray;
+    // GreenWim[] public contracts;
 
     mapping(address => uint) public balances;
-   // mapping(address => uint) public collateralBalances;
-   // mapping (address => uint) public hostAddressToContractId;
+    // mapping(address => uint) public collateralBalances;
+    // mapping (address => uint) public hostAddressToContractId;
 
-    event LeaseCreated(address indexed lessor, uint leaseId, uint noOfGWTokens, uint collateral);
+    event LeaseCreated(
+        address indexed lessor,
+        uint leaseId,
+        uint noOfGWTokens,
+        uint collateral
+    );
     event LeaseTaken(address indexed lessee, uint leaseId);
-    event LeaseEnded(address indexed lessee, uint leaseId, uint refundAmount, uint extraAmount);
+    event LeaseEnded(
+        address indexed lessee,
+        uint leaseId,
+        uint refundAmount,
+        uint extraAmount
+    );
 
     constructor(address _usdtAddress) {
-       // apiVerify = new APIVerify();
-       // gwToken = new GWToken();
+        // apiVerify = new APIVerify();
+        // gwToken = new GWToken();
         usdtToken = IERC20(_usdtAddress);
     }
 
     uint public credsMarketPrice;
-    
 
-    
-    function updateGWTokenBalance(address _walletAdd ,uint _newValue) public {
-        // additional cheks to be implemented 
+    function updateGWTokenBalance(address _walletAdd, uint _newValue) public {
+        // additional cheks to be implemented
         balances[_walletAdd] = _newValue;
-
-
     }
-
 
     // function addCredStation(string memory _code) public {
     //     apiVerify.callVerifier(_code);
@@ -77,20 +74,22 @@ contract Registry {
 
     function createSellOrder(uint _sellPrice, uint _noOfGWTokens) public {
         require(balances[msg.sender] >= _noOfGWTokens, "Insufficient GWTokens");
-        orderArray.push(Order({
-            seller: msg.sender,
-            owner: msg.sender,
-            orderId: orderArray.length,
-            sellPrice: _sellPrice,
-            isBuy:false,
-            isSale:true,
-            isLease:false,
-            leaseFee:0,
-            leaseDuration:0,
-            saleFulfilled: false,
-            leaseFulfilled:false,
-            noOfGWTokens: _noOfGWTokens
-        }));
+        orderArray.push(
+            Order({
+                seller: msg.sender,
+                owner: msg.sender,
+                orderId: orderArray.length,
+                sellPrice: _sellPrice,
+                isBuy: false,
+                isSale: true,
+                isLease: false,
+                leaseFee: 0,
+                leaseDuration: 0,
+                saleFulfilled: false,
+                leaseFulfilled: false,
+                noOfGWTokens: _noOfGWTokens
+            })
+        );
         balances[msg.sender] -= _noOfGWTokens;
     }
 
@@ -106,28 +105,34 @@ contract Registry {
         credsMarketPrice = order.sellPrice;
     }
 
-    function createLeaseOrder(uint _leasePrice, uint _noOfGWTokens, uint _collateral, uint256 _duration) public {
+    function createLeaseOrder(
+        uint _leasePrice,
+        uint _noOfGWTokens,
+        uint _collateral,
+        uint256 _duration
+    ) public {
         // duration to be implemented
         require(balances[msg.sender] >= _noOfGWTokens, "Insufficient GWTokens");
         //require(usdtToken.transferFrom(msg.sender, address(this), _collateral), "Collateral transfer failed");
-       
-     
-        orderArray.push(Order({
-            seller: msg.sender,
-            owner: msg.sender,
-            orderId: orderArray.length,
-            sellPrice: 0,
-            isBuy:false,
-            isSale:false,
-            isLease:true,
-            leaseFee:_leasePrice,
-            leaseDuration:_duration,
-            saleFulfilled: false,
-            leaseFulfilled:false,
-            noOfGWTokens: _noOfGWTokens
-        }));
+
+        orderArray.push(
+            Order({
+                seller: msg.sender,
+                owner: msg.sender,
+                orderId: orderArray.length,
+                sellPrice: 0,
+                isBuy: false,
+                isSale: false,
+                isLease: true,
+                leaseFee: _leasePrice,
+                leaseDuration: _duration,
+                saleFulfilled: false,
+                leaseFulfilled: false,
+                noOfGWTokens: _noOfGWTokens
+            })
+        );
         balances[msg.sender] -= _noOfGWTokens;
-       // emit LeaseCreated(msg.sender, leaseId, _noOfGWTokens, _collateral);
+        // emit LeaseCreated(msg.sender, leaseId, _noOfGWTokens, _collateral);
     }
 
     function takeOnLease(uint _orderId) public payable {
@@ -144,14 +149,11 @@ contract Registry {
 
     function endLease(uint _orderId) public payable {
         Order storage order = orderArray[_orderId];
-       // require(msg.value >= order.leaseFee, "Insufficient value sent");
+        // require(msg.value >= order.leaseFee, "Insufficient value sent");
         require(!order.leaseFulfilled, "Lease already fulfilled");
 
         order.owner = order.seller;
         order.leaseFulfilled = true;
         balances[msg.sender] -= order.noOfGWTokens;
-       
     }
-
-    
 }
