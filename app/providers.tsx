e7cef2ab56180ce2d5071ec0609f765d27aa0422
priyanshur66 <priyanshuranjan0001@gmail.com>
2024-02-27 21:53:18 +0530

    chains updated

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,48 +1,44 @@
-'use client';
+"use client";
 
-import * as React from 'react';
+import * as React from "react";
 import {
   RainbowKitProvider,
   getDefaultWallets,
   getDefaultConfig,
-} from '@rainbow-me/rainbowkit';
+} from "@rainbow-me/rainbowkit";
 import {
   argentWallet,
   trustWallet,
   ledgerWallet,
-} from '@rainbow-me/rainbowkit/wallets';
+} from "@rainbow-me/rainbowkit/wallets";
 import {
   arbitrum,
   base,
   mainnet,
+  opBNBTestnet,
   optimism,
   polygon,
   sepolia,
   zora,
-} from 'wagmi/chains';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
+} from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
 
 const { wallets } = getDefaultWallets();
 
 const config = getDefaultConfig({
-  appName: 'RainbowKit demo',
-  projectId: 'YOUR_PROJECT_ID',
+  appName: "RainbowKit demo",
+  projectId: "YOUR_PROJECT_ID",
   wallets: [
     ...wallets,
     {
-      groupName: 'Other',
+      groupName: "Other",
       wallets: [argentWallet, trustWallet, ledgerWallet],
     },
   ],
   chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    zora,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+    opBNBTestnet,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
   ],
   ssr: true,
 });
--- components/Navbar.js
+++ components/Navbar.js
@@ -6,45 +6,6 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function NavBar() {
   const [navbarOpen, setNavbarOpen] = useState(false);
-<<<<<<< HEAD
-
-    return (
-        <header class="fixed top-0 w-full clearNav z-50">
-        <div class="max-w-5xl mx-auto flex flex-wrap p-5 flex-col md:flex-row">
-          <div className="flex flex-row items-center justify-between p-3 md:p-1">
-            <Link
-              href="/"
-              class="flex text-3xl text-white font-medium mb-4 md:mb-0 "
-            >
-              CarbonCredits
-            </Link>
-  
-           
-          </div>
-          <div
-            className={
-              "md:flex flex-grow items-center" +
-              (navbarOpen ? " flex" : " hidden")
-            }
-          >
-            <div class="md:ml-auto md:mr-auto font-4 pt-1 md:pl-14 pl-1 flex flex-wrap items-center md:text-base text-1xl md:justify-center justify-items-start">
-              <Link
-                class="mr-12 md:ml-18 ml-0 cursor-pointer text-gray-300 hover:text-white text-2xl hover:text-l font-semibold tr04"
-                href="/lease"
-              >
-                Sale
-              </Link>
-              <Link
-                class="mr-12 md:ml-18 ml-0 cursor-pointer text-gray-300 hover:text-white text-2xl hover:text-l font-semibold tr04"
-                href="/lending"
-              >
-                Lending
-              </Link>
-              
-            </div>
-            <button className="border-2 border-white p-2 px-3 rounded-full">connect</button>
-          </div>
-=======
   const [flyer, setFlyer] = useState(false);
   const [flyerTwo, setFlyerTwo] = useState(false);
 
@@ -82,7 +43,6 @@ export default function NavBar() {
               <line x1="3" y1="18" x2="21" y2="18"></line>
             </svg>
           </button>
->>>>>>> 09aadaf (rainbowkit wallet integration)
         </div>
         <div
           className={
